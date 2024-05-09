e3b2b3c659a650336ac556325a41971d01044798
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-05-09 15:52:13 +0330

    define process_data that get json file from get_weather_data() function, and then extract time, sunrise and sunset. these time are timestamp format. For readable human , we use datatime module for convert tamestamp to date and time.At the end return date, time, sunrise and sunset.

--- get_weather_information.py
+++ get_weather_information.py
@@ -1,6 +1,5 @@
 import requests
-
-
+import datetime
 def get_weather_data():
     city = input('enter your city:')
     app_id = "88716c89a36e56e4398e8249f2ad0225"
@@ -17,4 +16,15 @@ def process_data(json_file):
     humidity = json_file['main']['humidity']
     city = json_file['name']
     return f'city:{city}, temp:{celsius_temp}, city:{city}, humidity:{humidity}'
+
+
+def process_time(json_file):
+    time = json_file['dt']
+    date_time = datetime.datetime.fromtimestamp(time)
+    sunrise = json_file['sys']['sunrise']
+    sunrise_time = datetime.datetime.fromtimestamp(sunrise).time()
+    sunset = json_file['sys']['sunset']
+    sunset_time = datetime.datetime.fromtimestamp(sunset).time()
+    return f'date: {date_time.date()}, time: {date_time.time()}, sunrise: {sunrise_time}, sunset: {sunset_time}'
+
 
