e73bdad5382343d3b94977d614e588c9cb31f85f
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-05-09 12:35:43 +0330

    define function that get city from user, then get api from openweather.org and return json file that  contains all information from city.

--- get_weather_information.py
+++ get_weather_information.py
@@ -1,1 +1,10 @@
 import requests
+
+
+def get_weather_data(city='Tehran'):
+    city = input('enter your city:')
+    app_id = "88716c89a36e56e4398e8249f2ad0225"
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city},&appid={app_id}'
+    weather_url = requests.get(url)
+    response = weather_url.json()
+    return response
