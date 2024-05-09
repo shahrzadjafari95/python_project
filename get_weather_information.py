0e83f9a1ef54d3082b7a3c69e145fe484633ba1a
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-05-09 15:12:08 +0330

    define process_data that get json file from get_weather_data function, and then extract temp, humidity, city form this file, and return these information.

--- get_weather_information.py
+++ get_weather_information.py
@@ -1,10 +1,20 @@
 import requests
 
 
-def get_weather_data(city='Tehran'):
+def get_weather_data():
     city = input('enter your city:')
     app_id = "88716c89a36e56e4398e8249f2ad0225"
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city},&appid={app_id}'
     weather_url = requests.get(url)
     response = weather_url.json()
     return response
+
+
+# show temp, humidity, city from json
+def process_data(json_file):
+    temp = json_file['main']['temp']
+    celsius_temp = round(temp - 273.15, 2)
+    humidity = json_file['main']['humidity']
+    city = json_file['name']
+    return f'city:{city}, temp:{celsius_temp}, city:{city}, humidity:{humidity}'
+
